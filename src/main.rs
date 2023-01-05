fcd7146c7e7fd27b1ae1fe1698dc105787d49ffe
Darric Heng <darricheng@gmail.com>
2023-01-05 21:20:19 +0800

    Update comments for port

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, Rust World!" }));
 
-    // run it with hyper on localhost:3000
+    // run it with hyper on localhost:10000
+    // See for why PORT 10000: https://community.render.com/t/express-port-issue-on-web-service/4061/2
     axum::Server::bind(&"0.0.0.0:10000".parse().unwrap())
         .serve(app.into_make_service())
         .await
