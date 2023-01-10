e965e6eb49f2dfe9c6e7c87e5f6f9190e14bcddb
Darric Heng <darricheng@gmail.com>
2023-01-10 23:14:23 +0800

    Update todo comment

--- src/db_config.rs
+++ src/db_config.rs
@@ -1,4 +1,4 @@
-// TODO: Add support for environment variables
+// TODO: Add support for environment variables (See main.rs for more info)
 
 pub const MONGO_URI: &str = "mongodb://localhost:27017/";
 pub const DB_NAME: &str = "financeTrackerApp";
