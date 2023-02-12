06f4c513b8aaca2145896a4459279a25eeaebc31
Darric Heng <darricheng@gmail.com>
2023-02-12 12:59:11 +0800

    Add middleware for cors requests

--- src/users/routes.rs
+++ src/users/routes.rs
@@ -1,12 +1,19 @@
 use super::controllers;
 use crate::db_config::get_mongodb_client;
 use axum::{
+    http::{header::CONTENT_TYPE, HeaderValue},
     routing::{delete, get, patch, post},
     Router,
 };
+use tower_http::cors::CorsLayer;
 
 pub async fn user_routes() -> Router {
     let mongodb_client = get_mongodb_client().await;
+
+    // TODO: Use environment variables for the allow_origin URL, so that it can be changed in production
+    let cors_layer = CorsLayer::new()
+        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+        .allow_headers([CONTENT_TYPE]);
 
     Router::new()
         .route("/add_user", post(controllers::add_user))
@@ -14,4 +21,5 @@ pub async fn user_routes() -> Router {
         .route("/update_user", patch(controllers::update_user))
         .route("/delete_user", delete(controllers::delete_user))
         .with_state(mongodb_client)
+        .layer(cors_layer)
 }
